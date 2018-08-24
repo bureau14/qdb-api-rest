6ce07d681fcd17103fd5b508a1c7b71fe0e3d39d
vianney <vianney.plotton@gmail.com>
2018-08-24 14:18:14 +0200

    Case 2166 - Add versionning.

---
+++ scripts/teamcity/package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$#" -ne 3 ]; then
+    echo "Usage: package /path/to/qdb-api-rest-server/binary /path/to/swagger.json os_name"
+    exit
+fi
+
+EXE_PATH=$1;shift
+SWAGGER_PATH=$1;shift
+OS=$1;shift
+
+VERSION=`cat $SWAGGER_PATH | grep "\"version\":" | awk -F '"' '{print $4}'`
+
+tar cvzf qdb-$VERSION-$OS-api-rest.tar.gz $EXE_PATH
