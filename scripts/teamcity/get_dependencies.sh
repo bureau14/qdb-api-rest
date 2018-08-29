66fdd1a731cc7fb1d31b9d1bc0e69a034bba39ba
vianney <vianney.plotton@gmail.com>
2018-08-29 08:12:45 +0200

    Main shall be excluded from generation.

--- scripts/teamcity/get_dependencies.sh
+++ scripts/teamcity/get_dependencies.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 go get github.com/go-swagger/go-swagger/cmd/swagger
-swagger generate server -f ./swagger.json -A qdb-api-rest -P models.Principal
+swagger generate server -f ./swagger.json -A qdb-api-rest -P models.Principal --exclude-main
 
 cd $1
 mv configure_qdb_api_rest.go restapi/configure_qdb_api_rest.go
