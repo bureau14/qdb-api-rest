16738385ec8c13d179c2db05128661599ebf5cc2
vianney <vianney.plotton@gmail.com>
2018-09-04 14:40:07 +0200

    Initialize server in main.

--- apps/qdb_rest_service/main.go
+++ apps/qdb_rest_service/main.go
@@ -26,41 +26,6 @@ func (p *program) Start(s service.Service) error {
 }
 
 func (p *program) run() {
-	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	api := operations.NewQdbAPIRestAPI(swaggerSpec)
-	p.server = restapi.NewServer(api)
-
-	parser := flags.NewParser(p.server, flags.Default)
-	parser.ShortDescription = "QuasarDB API"
-	parser.LongDescription = "Find out more at https://doc.quasardb.net"
-
-	p.server.ConfigureFlags()
-	for _, optsGroup := range api.CommandLineOptionsGroups {
-		_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
-
-	if _, err := parser.Parse(); err != nil {
-		code := 1
-		if fe, ok := err.(*flags.Error); ok {
-			if fe.Type == flags.ErrHelp {
-				code = 0
-			}
-		}
-		os.Exit(code)
-	}
-
-	p.server.ConfigureAPI()
-	// this must be done after the api has been configured
-	p.server.TLSHost = restapi.APIConfig.TLSHost
-	p.server.TLSPort = restapi.APIConfig.TLSPort
-
 	if err := p.server.Serve(); err != nil {
 		log.Fatalln(err)
 	}
@@ -83,7 +48,42 @@ func main() {
 		Description: "This is quasardb rest service.",
 	}
 
+	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	prg := &program{}
+	api := operations.NewQdbAPIRestAPI(swaggerSpec)
+	prg.server = restapi.NewServer(api)
+
+	parser := flags.NewParser(prg.server, flags.Default)
+	parser.ShortDescription = "QuasarDB API"
+	parser.LongDescription = "Find out more at https://doc.quasardb.net"
+
+	prg.server.ConfigureFlags()
+	for _, optsGroup := range api.CommandLineOptionsGroups {
+		_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if _, err := parser.Parse(); err != nil {
+		code := 1
+		if fe, ok := err.(*flags.Error); ok {
+			if fe.Type == flags.ErrHelp {
+				code = 0
+			}
+		}
+		os.Exit(code)
+	}
+
+	prg.server.ConfigureAPI()
+	// this must be done after the api has been configured
+	prg.server.TLSHost = restapi.APIConfig.TLSHost
+	prg.server.TLSPort = restapi.APIConfig.TLSPort
+
 	s, err := service.New(prg, svcConfig)
 	if err != nil {
 		log.Fatal(err)
