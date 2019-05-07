d45f3e6bd00d6b720e715b346614dae5fce03907
Marek Kurdej <marek@quasardb.net>
2019-05-07 15:04:07 +0200

    Bump version to 3.3.0.0.

--- configure_qdb_api_rest.go
+++ configure_qdb_api_rest.go
@@ -38,7 +38,7 @@ func configureFlags(api *operations.QdbAPIRestAPI) {
 
 var defaultSecret = []byte("default_secret")
 
-const version string = "3.3.0master"
+const version string = "3.3.0"
 
 func configureAPI(api *operations.QdbAPIRestAPI) http.Handler {
 
