23d04e1a4a2e240a10acedf034c6d377898bd42c
vianney <vianney.p@outlook.com>
2020-01-27 10:24:05 +0100

    Bump version to 3.6.0-rc2.

--- restapi/doc.go
+++ restapi/doc.go
@@ -10,7 +10,7 @@ Find out more at https://doc.quasardb.net
       https
     Host: localhost
     BasePath: /api
-    Version: 3.6.0-nightly.0
+    Version: 3.6.0-rc.2
 
     Consumes:
     - application/json
