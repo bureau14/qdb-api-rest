a397ab8a85b65eda32984e7f517f7d0123655f1e
vianney <vianney@quasardb.net>
2020-03-04 10:14:04 +0100

    Bump version to 3.8.0.0-nightly.0.

--- restapi/configure_qdb_api_rest.go
+++ restapi/configure_qdb_api_rest.go
@@ -49,7 +49,7 @@ func configureFlags(api *operations.QdbAPIRestAPI) {
 
 var secret *rsa.PrivateKey
 
-const version string = "3.7.0-nightly.1"
+const version string = "3.8.0-nightly.0"
 
 func excelSerialNumber(t time.Time) float64 {
 	return (float64(t.UTC().Unix()) / 86400) + 25569
--- restapi/doc.go
+++ restapi/doc.go
@@ -10,7 +10,7 @@ Find out more at https://doc.quasardb.net
       https
     Host: localhost
     BasePath: /api
-    Version: 3.7.0-nightly.1
+    Version: 3.8.0-nightly.0
 
     Consumes:
     - application/json
