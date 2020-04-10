589a5b6ce370735eec7bc55d0cd7a29dad8b5ccb
terngkub <terngnattapol@gmail.com>
2020-04-10 15:15:33 +0200

    Bump version to 3.9.0.0-nightly.0.

--- restapi/doc.go
+++ restapi/doc.go
@@ -10,7 +10,7 @@ Find out more at https://doc.quasardb.net
       https
     Host: localhost
     BasePath: /api
-    Version: 3.8.0-nightly.0
+    Version: 3.9.0-nightly.0
 
     Consumes:
     - application/json
