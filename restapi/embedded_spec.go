c0a88c33879151aec694c1af0f0593b444a77f30
vianney <vianney.p@outlook.com>
2020-09-25 09:21:27 +0200

    Bump version to 3.9.4.

--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -32,7 +32,7 @@ func init() {
   "info": {
     "description": "Find out more at https://doc.quasardb.net",
     "title": "QuasarDB API",
-    "version": "3.9.4-nightly.0"
+    "version": "3.9.4"
   },
   "basePath": "/api",
   "paths": {
@@ -612,7 +612,7 @@ func init() {
   "info": {
     "description": "Find out more at https://doc.quasardb.net",
     "title": "QuasarDB API",
-    "version": "3.9.4-nightly.0"
+    "version": "3.9.4"
   },
   "basePath": "/api",
   "paths": {
