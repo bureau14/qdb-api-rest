9c766ada8d89a24ee2f911d5a4dfb7729c7ebf56
vianney <vianney.p@outlook.com>
2021-01-06 10:55:03 +0100

    Bump version to 3.8.11-nightly.

--- restapi/configure_qdb_api_rest.go
+++ restapi/configure_qdb_api_rest.go
@@ -51,7 +51,7 @@ func configureFlags(api *operations.QdbAPIRestAPI) {
 
 var secret *rsa.PrivateKey
 
-const version string = "3.8.10-nightly.0"
+const version string = "3.8.11-nightly.0"
 
 func excelSerialNumber(t time.Time) float64 {
 	return (float64(t.UTC().Unix()) / 86400) + 25569
