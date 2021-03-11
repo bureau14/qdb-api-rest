0c372b9c4b5d303399f142072ca783ab6905581c
vianney <vianney.p@outlook.com>
2021-03-11 10:51:40 +0100

    Add gzip encoding capability.

--- tests/smoke-test.sh
+++ tests/smoke-test.sh
@@ -2,20 +2,36 @@
 
 set -xe
 
-TOKEN=`curl -k -H "Content-Type: application/json" -X POST --data-binary @empty.private http://127.0.0.1:40080/api/login | grep -o '"token": *"[^"]*"' | grep -o ': *"[^"]*"' | grep -o '"[^"]*"' | tr -d '"'`
+function login {
+    curl -k -H "Content-Type: application/json" -X POST --data-binary @empty.private http://127.0.0.1:40080/api/login | grep -o '"token": *"[^"]*"' | grep -o ': *"[^"]*"' | grep -o '"[^"]*"' | tr -d '"'
+}
 
-echo "token: $TOKEN"
+
+TOKEN=$(login)
+
 
 curl -k -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -sb -X POST -d '{"query":"CREATE TABLE ts(col int64)"}' http://127.0.0.1:40080/api/query
 curl -k -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -sb -X POST -d '{"query":"INSERT INTO ts($timestamp, col) VALUES(2017-01-01, 1)"}' http://127.0.0.1:40080/api/query
 curl -k -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -sb -X POST -d '{"query":"INSERT INTO ts($timestamp, col) VALUES(2017-01-02, 2)"}' http://127.0.0.1:40080/api/query
 
+
 EXPECTED='{"tables":[{"columns":[{"data":[2],"name":"count(col)","type":"count"}]}]}'
+ERROR=0
+
+# Without encoding
 RESULT=`curl -k -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -sb -X POST -d '{"query":"select count(*) from ts in range (2017,+1y)"}' http://127.0.0.1:40080/api/query`
+if [ $EXPECTED != $RESULT ]; then
+    echo "Result does not match without encoding."
+    ERROR=1
+fi
+
+# With encoding
+RESULT=`curl -k -H "Accept-Encoding: gzip" -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -sb -X POST -d '{"query":"select count(*) from ts in range (2017,+1y)"}' http://127.0.0.1:40080/api/query | gunzip`
+if [ $EXPECTED != $RESULT ]; then
+    echo "Result does not match with encoding."
+    ERROR=1
+fi
 
 curl -k -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -sb -X POST -d '{"query":"DROP TABLE ts"}' http://127.0.0.1:40080/api/query
 
-if [ $EXPECTED != $RESULT ]; then
-    echo "Result does not match!"
-    exit 1
-fi
+exit $ERROR
