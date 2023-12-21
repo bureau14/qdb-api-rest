2291ff44105fe915a63c64f6b024abd3d8ef974e
terngkub <terngnattapol@gmail.com>
2023-12-21 23:01:41 +0700

    Bump version to 3.14.1.

--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -32,7 +32,7 @@ func init() {
   "info": {
     "description": "Find out more at https://doc.quasardb.net",
     "title": "QuasarDB API",
-    "version": "3.14.1-nightly.0"
+    "version": "3.14.1"
   },
   "basePath": "/api",
   "paths": {
@@ -696,7 +696,7 @@ func init() {
   "info": {
     "description": "Find out more at https://doc.quasardb.net",
     "title": "QuasarDB API",
-    "version": "3.14.1-nightly.0"
+    "version": "3.14.1"
   },
   "basePath": "/api",
   "paths": {
