8d904f46a7ce166d7ddbabebface03c867a9f58c
Igor <igor.niebylski@gmail.com>
2025-01-13 10:34:04 +0100

    remove stats which are no longer available, fix package

--- qdbinterface/cluster_information.go
+++ qdbinterface/cluster_information.go
@@ -34,8 +34,6 @@ func shouldUpdate() bool {
 }
 
 func resetInformation() {
-	ClusterInformation.DiskTotal = new(int64)
-	ClusterInformation.DiskUsed = new(int64)
 	ClusterInformation.MemoryTotal = new(int64)
 	ClusterInformation.MemoryUsed = new(int64)
 	ClusterInformation.Nodes = []string{}
@@ -59,32 +57,18 @@ func RetrieveInformation(handle qdb.HandleType) error {
 		*ClusterInformation.Status = "unstable"
 	}
 
-	diskTotal := int64(0)
-	diskUsed := int64(0)
 	memoryTotal := int64(0)
 	memoryUsed := int64(0)
 
 	ClusterInformation.Nodes = []string{}
 	NodesInformation = make(map[string]models.Node)
 	for _, stat := range stats {
-		diskUsedByNode := stat.Disk.BytesTotal - stat.Disk.BytesFree
-		diskTotal += stat.Disk.BytesTotal
-		diskUsed += diskUsedByNode
 		memoryTotal += stat.Memory.Physmem.Total
 		memoryUsed += stat.Memory.Physmem.Used
 		ClusterInformation.Nodes = append(ClusterInformation.Nodes, stat.NodeID)
 
 		id := stat.NodeID
 		node := models.Node{}
-		cpuUser := int64(stat.CPU.User)
-		cpuSystem := int64(stat.CPU.System)
-		cpuIdle := stat.CPU.Idle
-		cpuUsed := cpuUser + cpuSystem
-		cpuTotal := cpuUsed + cpuIdle
-		node.CPUTotal = &cpuTotal
-		node.CPUUsed = &cpuUsed
-		node.DiskTotal = &stat.Disk.BytesTotal
-		node.DiskUsed = &diskUsedByNode
 		node.ID = &id
 		node.MemoryTotal = &stat.Memory.Physmem.Total
 		node.MemoryUsed = &stat.Memory.Physmem.Used
@@ -92,8 +76,6 @@ func RetrieveInformation(handle qdb.HandleType) error {
 		node.QuasardbVersion = &stat.EngineVersion
 		NodesInformation[id] = node
 	}
-	ClusterInformation.DiskTotal = &diskTotal
-	ClusterInformation.DiskUsed = &diskUsed
 	ClusterInformation.MemoryTotal = &memoryTotal
 	ClusterInformation.MemoryUsed = &memoryUsed
 	return err
