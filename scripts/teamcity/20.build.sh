b6057c5f2ba6e80c389cc4c2db95d6990e022cb6
Aleksey Alekseev <voodoo144@gmail.com>
2025-05-27 22:15:32 -0400

    SC-15875: using build time from last git commit

--- scripts/teamcity/20.build.sh
+++ scripts/teamcity/20.build.sh
@@ -13,7 +13,7 @@ ${SET_VERSION_SCRIPT} 3.15.0
 git config --global --add safe.directory '*'
 
 sed -i -e 's/const GitHash string = .*/const GitHash string = "'${GIT_HASH}'"/' ${BASE_DIR}/meta/version.go
-sed -i -e 's/const BuildTime string = .*/const BuildTime string = "'${CURRENT_DATETIME}'"/' ${BASE_DIR}/meta/version.go
+sed -i -e 's/const BuildTime string = .*/const BuildTime string = "'${GIT_CURRENT_COMMIT_TIME}'"/' ${BASE_DIR}/meta/version.go
 sed -i -e 's/const GoVersion string = .*/const GoVersion string = "'${GO_COMPILER_VERSION}'"/' ${BASE_DIR}/meta/version.go
 sed -i -e 's/const Platform string = .*/const Platform string = "'${PLATFORM}'"/' ${BASE_DIR}/meta/version.go
 
