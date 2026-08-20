cc7f20fafc316fc0aad6c5cae9311d7e1e62233f
Leon Mergen <leon@solatis.com>
2026-08-20 13:37:51 +0700

    test(bench): add new-rest server and flightsql protocol stubs

---
+++ tests/e2e/bench/protocols/flightsql.py
@@ -0,0 +1,7 @@
+"""Arrow Flight SQL protocol: the gateway thesis measured end to end."""
+
+TTFB_DEFINITION = "arrival of the first Arrow record batch"
+
+
+def fetch(cfg, query, record_ttfb, telemetry):
+    raise NotImplementedError("available with M3")
